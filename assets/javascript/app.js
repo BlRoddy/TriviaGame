c69eede7990ea80dca45a72a3301ae7f27f822a2
Blake Roddy <you@example.com>
2016-08-31 19:12:34 -0500

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,23 @@
+$('#startButton').on('click', function() {
+    game.begin();
+})
+
+
 var questions = {
+		question: "What song did Elton John play with Miss Piggy and the Electric Mayhem in season 2 of The Muppet Show?",
+		choices:["Rocket Man", "Don't go Breaking my Heart", "Crocodile Rock", "Bennie & the Jets"],
+		answer:"Don't go Breaking my Heart"
+	},
+	{
+		question:"What instrument does Janice, the only female in Electric Mayhem, play?",
+		choices:["Keybaords", "Bass", "Guitar", "Harmonica"],
+		answer:"Guitar"
+	},
+	{
+		question:"What are the names of the ever-heckling curmudgeons who sit in the balcony?"
+		choices:["Statler & Waldorf","Bert & Ernie","Enoch & Salinger",]
+		answer:"Statler & Waldorf"
+	};
 
 
 
-
-
-
-
-	
-}
